aba4468f8abc59346bea8795ea95e9ae6daccbb9
aB-temps <187414947+aB-temps@users.noreply.github.com>
2025-10-27 09:51:38 +0100

    ➕ add(background): some bg

--- nvim/.config/nvim/lua/plugins/live-preview.lua
+++ nvim/.config/nvim/lua/plugins/live-preview.lua
@@ -3,4 +3,11 @@ return {
     dependencies = {
 		'folke/snacks.nvim',
     },
+	opts = {
+		port = 4242,
+		browser = 'default',
+		dynamic_root = false,
+		sync_scroll = true,
+		picker = "snacks.picker"
+	},
 }

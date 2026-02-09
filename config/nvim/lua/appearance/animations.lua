21c0308b9638f20d6d3c37ab81d3bae80a63c3d5
leonardecavele <leonard.decavele@gmail.com>
2026-02-09 17:10:33 +0000

    edit: color of tinyglimmer

--- config/nvim/lua/appearance/animations.lua
+++ config/nvim/lua/appearance/animations.lua
@@ -1,5 +1,16 @@
 -- https://github.com/rachartier/tiny-glimmer.nvim
 local tiny_glimmer_ok, tiny_glimmer = pcall(require, "tiny-glimmer")
 if tiny_glimmer_ok then
-    tiny_glimmer.setup()
+    tiny_glimmer.setup({
+        animations = {
+            fade = {
+                from_color = "#f0e056",
+                to_color = "#00e05f",
+            },
+        },
+        overwrite = {
+            yank = { enabled = true, default_animation = "fade" },
+            paste = { enabled = true, default_animation = "fade" },
+        },
+    })
 end
